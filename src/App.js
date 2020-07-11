c02e376f60dde823419770ef68476a72fb77ec62
vanilla-unicorn <vanilla_unicorn@via.tokyo.jp>
2020-07-11 11:46:48 +0900

    ADDED: core form example with fields

--- src/App.js
+++ src/App.js
@@ -1,26 +1,80 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {
+	Container,
+	Label,
+	Input,
+	Button,
+	Form,
+	FormGroup,
+	FormFeedback
+} from 'reactstrap';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	constructor(props) {
+		super(props);
+		this.handleSubmit = this.handleSubmit.bind(this);
+		this.state = {};
+	}
+
+	handleSubmit(event) {
+		event.preventDefault();
+		const form = event.target;
+		console.log(form);
+	}
+
+	render() {
+		const inputStyle = {
+			backgroundColor: '#2E4053',
+			color: '#FFFFFF',
+			fontWeight: 'bold',
+			border: '1px solid #1069D9'
+		};
+
+		return (
+			<Container className="App-header text-center" fluid>
+				<h1>Form Validation Example Signup</h1>
+				<hr className="w-100" />
+				<Form className="mt-3" onSubmit={this.handleSubmit}>
+					<Container>
+						<FormGroup>
+							<Label htmlFor="username">Username:</Label>
+							<Input
+								name="username"
+								style={inputStyle}
+								valid={false}
+								invalid={false}
+							/>
+						</FormGroup>
+						<FormGroup>
+							<Label htmlFor="email">Email:</Label>
+							<Input
+								name="email"
+								style={inputStyle}
+								valid={false}
+								invalid={false}
+							/>
+						</FormGroup>
+						<FormGroup>
+							<Label htmlFor="password">Password:</Label>
+							<Input
+								name="password"
+								type="password"
+								style={inputStyle}
+								valid={false}
+								invalid={false}
+							/>
+							<FormFeedback invalid>minimum 8 characters</FormFeedback>
+						</FormGroup>
+					</Container>
+					<Button color="primary" size="lg" className="mt-3 w-75">
+						Submit
+					</Button>
+				</Form>
+			</Container>
+		);
+	}
 }
 
 export default App;
