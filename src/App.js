48fd2421e9bb8ebfd69db81702163a976dacddb8
vanilla-unicorn <vanilla_unicorn@via.tokyo.jp>
2020-07-11 15:36:08 +0900

    ADDED: logic that attempts to check if the fields are empty when information is entered and determines whether to notify of error or success

--- src/App.js
+++ src/App.js
@@ -15,13 +15,50 @@ class App extends Component {
 	constructor(props) {
 		super(props);
 		this.handleSubmit = this.handleSubmit.bind(this);
-		this.state = {};
+		this.handleChange = this.handleChange.bind(this);
+		this.state = {
+			username: {
+				valid: false,
+				invalid: false
+			},
+			email: {
+				valid: false,
+				invalid: false
+			},
+			password: {
+				valid: false,
+				invalid: false
+			},
+			verify: {
+				valid: false,
+				invalid: false
+			}
+		};
 	}
 
 	handleSubmit(event) {
 		event.preventDefault();
 		const form = event.target;
 		console.log(form);
+	}
+
+	handleChange(event) {
+		event.preventDefault();
+		if (event.target.value.length === 0) {
+			this.setState({
+				[event.target.name]: {
+					valid: false,
+					invalid: true
+				}
+			});
+		} else {
+			this.setState({
+				[event.target.name]: {
+					valid: true,
+					invalid: false
+				}
+			});
+		}
 	}
 
 	render() {
@@ -36,25 +73,40 @@ class App extends Component {
 			<Container className="App-header text-center" fluid>
 				<h1>Form Validation Example Signup</h1>
 				<hr className="w-100" />
-				<Form className="mt-3" onSubmit={this.handleSubmit}>
-					<Container>
+				<Form className="mt-3 w-100" onSubmit={this.handleSubmit}>
+					<Container className="w-25">
 						<FormGroup>
 							<Label htmlFor="username">Username:</Label>
 							<Input
 								name="username"
 								style={inputStyle}
-								valid={false}
-								invalid={false}
+								valid={this.state.username.valid}
+								invalid={this.state.username.invalid}
+								onChange={this.handleChange}
 							/>
+							<FormFeedback invalid={this.state.username.invalid.toString()}>
+								required
+							</FormFeedback>
+							<FormFeedback valid={this.state.username.valid.toString()}>
+								Great!
+							</FormFeedback>
 						</FormGroup>
 						<FormGroup>
 							<Label htmlFor="email">Email:</Label>
 							<Input
 								name="email"
+								type="email"
 								style={inputStyle}
-								valid={false}
-								invalid={false}
+								valid={this.state.email.valid}
+								invalid={this.state.email.invalid}
+								onChange={this.handleChange}
 							/>
+							<FormFeedback invalid={this.state.email.invalid.toString()}>
+								required
+							</FormFeedback>
+							<FormFeedback valid={this.state.email.valid.toString()}>
+								Great!
+							</FormFeedback>
 						</FormGroup>
 						<FormGroup>
 							<Label htmlFor="password">Password:</Label>
@@ -62,10 +114,16 @@ class App extends Component {
 								name="password"
 								type="password"
 								style={inputStyle}
-								valid={false}
-								invalid={false}
+								valid={this.state.password.valid}
+								invalid={this.state.password.invalid}
+								onChange={this.handleChange}
 							/>
-							<FormFeedback invalid>minimum 8 characters</FormFeedback>
+							<FormFeedback invalid={this.state.password.invalid.toString()}>
+								required
+							</FormFeedback>
+							<FormFeedback valid={this.state.password.valid.toString()}>
+								Great!
+							</FormFeedback>
 						</FormGroup>
 						<FormGroup>
 							<Label htmlFor="verify">Verify Password:</Label>
@@ -73,12 +131,22 @@ class App extends Component {
 								name="verify"
 								type="password"
 								style={inputStyle}
-								valid={false}
-								invalid={false}
+								valid={this.state.verify.valid}
+								invalid={this.state.verify.invalid}
+								onChange={this.handleChange}
 							/>
+							<FormFeedback invalid={this.state.verify.invalid.toString()}>
+								required
+							</FormFeedback>
+							<FormFeedback invalid={this.state.verify.invalid.toString()}>
+								passwords must match
+							</FormFeedback>
+							<FormFeedback valid={this.state.verify.valid.toString()}>
+								Great!
+							</FormFeedback>
 						</FormGroup>
 					</Container>
-					<Button color="primary" size="lg" className="mt-3 w-75">
+					<Button color="primary" size="lg" className="mt-3 w-25">
 						Submit
 					</Button>
 				</Form>
